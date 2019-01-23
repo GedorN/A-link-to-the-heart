c9f24c0fb403aeba142e06a976af7f2e4d8b555b
GedorN <gedor.silvaneto@gmail.com>
2019-01-22 23:28:14 -0200

    Correção de parâmetros passados para a classe Base

--- src/Background.lua
+++ src/Background.lua
@@ -1,7 +1,7 @@
 Background = Entity:extend()
 
 function Background:new(imgPath, x, y)
-  Background.super.new(self, imgPath, x, y, 0, 0, 0, 0)
+  Background.super.new(self, imgPath, x, y, 514, 402, 0, 1)
   width = self.sprite:getWidth()
   height = self.sprite:getHeight()
 end
