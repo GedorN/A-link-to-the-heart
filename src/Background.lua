54fa38c07d7cbc281391fc1446ad6a07e98280c5
rul3rst4 <37764417+rul3rst4@users.noreply.github.com>
2019-01-24 06:18:39 -0200

    back

--- src/Background.lua
+++ src/Background.lua
@@ -1,11 +1,7 @@
 Background = Object:extend()
 
 function Background:new(imgPath, x, y)
-<<<<<<< Updated upstream
-  Background.super.new(self, imgPath, x, y, 800, 600, 0, 2)
-=======
   self.sprite = love.graphics.newImage(imgPath)
->>>>>>> Stashed changes
   width = self.sprite:getWidth()
   height = self.sprite:getHeight()
   self.x, self.y = x, y
