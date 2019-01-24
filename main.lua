077f072c58f73227b59e21bc608730e8725cf5d1
rul3rst4 <37764417+rul3rst4@users.noreply.github.com>
2019-01-24 06:12:04 -0200

    Camera funcionando.

--- main.lua
+++ main.lua
@@ -1,11 +1,24 @@
+function camera_track(x, y)
+  w = (width / 2) / Zoom
+  h = (height / 2) / Zoom
+  if x - w >= 0 and x + w <= width then
+    camera:lockX(x, Camera.smooth.damped(2))
+  end
+
+  if y - h >= 0 and y + h <= height then
+    camera:lockY(y, Camera.smooth.damped(2))
+  end
+end
+
 function love.load()
+  --Require--
   Object = require("src.classic")
-  Camera = require ("src.Camera")
   require("src.Entity")
   require("src.Character")
   require("src.Enemy")
   require("src.Player")
   require("src.Background")
+<<<<<<< Updated upstream
   love.graphics.setDefaultFilter('nearest', 'nearest')
   canvas = love.graphics.newCanvas(800, 600)
   camera = Camera(200, 150, 400, 300)
@@ -18,21 +31,59 @@ function love.load()
   love.window.setFullscreen(true)
   x = 0
   y = 0
-end
+=======
+  push = require("src.push")
+  --Require--
+  local _, _, flags = love.window.getMode()
+  width, height = love.window.getDesktopDimensions(flags.display) -- flags.display contem o monitor que esta sendo usado.
+  love.graphics.setDefaultFilter('nearest', 'nearest') -- Filtro para que o render não use blur e deixe um aspecto pixelado.
 
+  Camera = require("src.Camera") -- Camera.
+  push:setupScreen(width, height, width, height, {fullscreen = true}) -- GameScreen Settings.
+
+  Zoom = 2
+  bg = Background("img/hh.png", 0, 0)
+  camera = Camera(0, 0, Zoom)
+
+  --- Abaixo apagar quando personagem estiver pronto.
+  x = 960
+  y = 540
+  camera:lookAt(x, y)
+>>>>>>> Stashed changes
+end
+--[[  Anotação sobre a Camera:
+O tamanho da camera é calculado considerando o width e height dividido por 2 respectivamente,
+isso pois ele divide o ponto a ser focado em 4 eixos a partir dele. Ou seja, um zoom de 2 em
+uma resolução de 1920 considerando o eixo X significa dividir em 2 partes, 960 a direita do ponto
+e 960 a esquerda, então dividir esse valor por 2(que é o Zoom efetivamente), o que resulta em 480 que é a largura da camera.
+Basicamente para calcular o tamanho da camera fazemos: (Tamanho TOTAL)
+x = GameWidth
+y = GameHeight
+camSizeX = x / Zoom
+camsizeY = y / Zoom
+]]
+--[[ Dica, efeito interessante: Iniciar a camera em uma posição aleatoria e em update()
+colocar para seguir um objeto, gera efeito de movimento da camera até o objeto. Focando.
+]]
 function love.update(dt)
+<<<<<<< Updated upstream
   camera:update(dt)
   p:update(dt, frames)
   camera:follow(p:getCoordinateX(), p:getCoordinateY())
+=======
+>>>>>>> Stashed changes
   require("src.lurker").update()
+  camera_track(x, y)
+
   if love.keyboard.isDown("w") then
     y = y - 100 * dt
   end
-  if love.keyboard.isDown("a") then
-    x = x - 100 * dt
-  end
   if love.keyboard.isDown("s") then
     y = y + 100 * dt
+  end
+
+  if love.keyboard.isDown("a") then
+    x = x - 100 * dt
   end
   if love.keyboard.isDown("d") then
     x = x + 100 * dt
@@ -42,20 +93,20 @@ end
 
 
 function love.draw()
-  love.graphics.setCanvas(canvas)
-  love.graphics.clear()
+  push:start()
   camera:attach()
+<<<<<<< Updated upstream
   bg:draw()
   -- draw aqui.
   p:draw()
+=======
+  -- Inicio Draw.
+  bg:draw()
+  love.graphics.points(x, y)
+  -- Fim Draw.
+>>>>>>> Stashed changes
   camera:detach()
-  love.graphics.setCanvas()
-
-  love.graphics.setColor(1, 1, 1, 1)
-  love.graphics.setBlendMode('alpha', 'premultiplied')
-  love.graphics.draw(canvas, 0, 0, 0, 2, 2)
-  love.graphics.setBlendMode('alpha')
-
+  push:finish()
 end
 
 function love.keypressed(key)
--- src/Background.lua
+++ src/Background.lua
@@ -1,7 +1,16 @@
-Background = Entity:extend()
+Background = Object:extend()
 
 function Background:new(imgPath, x, y)
+<<<<<<< Updated upstream
   Background.super.new(self, imgPath, x, y, 800, 600, 0, 2)
+=======
+  self.sprite = love.graphics.newImage(imgPath)
+>>>>>>> Stashed changes
   width = self.sprite:getWidth()
   height = self.sprite:getHeight()
+  self.x, self.y = x, y
+end
+
+function Background:draw()
+  love.graphics.draw(self.sprite, self.x, self.y)
 end
